ef0fe59524b8d3bc5e8df6c19e839da6084d1346
Zac Heisey <zheisey@gmail.com>
2018-11-13 16:45:29 -0500

    work on components

--- main.js
+++ main.js
@@ -3,18 +3,38 @@ var app = document.querySelector('[data-app]');
 // Determine the view/UI
 var page = app.getAttribute('data-app');
 
-//========================================//
 // Create new component via Reef.js
 var todoApp = new Reef(app, {
   data: {
     createListBtnText: 'Create a New Todo List',
-    myListBtnText: 'See My Lists'
+    myListBtnText: 'View My Lists'
   },
   template: function (data) {
-    var html = '<button>' + data.createListBtnText + '</button><button>' + data.myListBtnText + '</button>';
+    var html = '<button id="create-list">' + data.createListBtnText + '</button><button id="view-lists">' + data.myListBtnText + '</button>';
     return html;
   }
 });
 
 // Render Todo App
 todoApp.render();
+
+// Create new list
+var createList = function() {
+  // code block TBD
+}
+
+// Show existing lists
+var showLists = function() {
+  // code block TBD
+};
+
+document.addEventListener('click', function(event) {
+  // Create new list
+  if (event.target.id === 'create-list') {
+    // code block TBD
+  }
+  // View existing lists
+  if (event.target.id === 'view-lists') {
+    location.assign('/lists/');
+  }
+});
