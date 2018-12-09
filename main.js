003f39fd4a9d835656a7a5f2377d4bac0602e3f1
Zac Heisey <zheisey@gmail.com>
2018-12-09 13:57:32 -0500

    remove code that updates checked items markup in localStorage

--- main.js
+++ main.js
@@ -133,8 +133,5 @@ document.addEventListener('click', function(event) {
   if (event.target.type === 'checkbox') {
     // Toggle 'checked' class
     event.target.nextSibling.classList.toggle('checked');
-    // Update markup in localStorage
-    getSavedListItems.push({ list: params['list-name'], markup: createdLists.innerHTML });
-    localStorage.setItem('savedItems', JSON.stringify(getSavedListItems));
   }
 }, false);
