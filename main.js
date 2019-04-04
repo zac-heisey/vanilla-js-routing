096588b66ae7e81f51f49d11744bf4040fcbe01e
Zac Heisey <zheisey@gmail.com>
2019-04-04 12:10:16 -0400

    comment out pencil edit functionality

--- main.js
+++ main.js
@@ -132,22 +132,22 @@ document.addEventListener('click', function(event) {
     createdLists.innerHTML = '';
   }
 
-  // If click is on pencil icon
-  if (event.target.classList.contains('edit-item')) {
-    var pencil = event.target;
-    pencil.previousSibling.lastElementChild.setAttribute('contenteditable', true);
-    pencil.previousSibling.lastElementChild.focus();
-    // Loop through list items & update edited todo items
-    for (var i = 0; i < getSavedListItems.length; i++) {
-      getSavedListItems[i].items.forEach(function(item) {
-        if (pencil.previousSibling.lastElementChild.innerText !== item.item) {
-          item.item = pencil.previousSibling.lastElementChild.innerText;
-        }
-        // Update localStorage
-        localStorage.setItem('savedItems', JSON.stringify(getSavedListItems));
-      });
-    }
-  }
+  // If click is on pencil icon => NEED TO FIX
+  // if (event.target.classList.contains('edit-item')) {
+  //   var pencil = event.target;
+  //   pencil.previousSibling.lastElementChild.setAttribute('contenteditable', true);
+  //   pencil.previousSibling.lastElementChild.focus();
+  //   // Loop through list items & update edited todo items
+  //   for (var i = 0; i < getSavedListItems.length; i++) {
+  //     getSavedListItems[i].items.forEach(function(item) {
+  //       if (pencil.previousSibling.lastElementChild.innerText !== item.item) {
+  //         item.item = pencil.previousSibling.lastElementChild.innerText;
+  //       }
+  //       // Update localStorage
+  //       localStorage.setItem('savedItems', JSON.stringify(getSavedListItems));
+  //     });
+  //   }
+  // }
 
   // If click is on checkbox input
   if (event.target.type === 'checkbox') {
